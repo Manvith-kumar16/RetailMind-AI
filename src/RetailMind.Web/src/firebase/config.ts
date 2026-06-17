8c969a91d4bdae99a71fbcb418f1bec8d0d9e90e
Manvith-Kumar16 <manvithkumar1319@gmail.com>
2026-06-17 19:46:46 +0530

    feat: implemented email sign-up

--- src/RetailMind.Web/src/firebase/config.ts
+++ src/RetailMind.Web/src/firebase/config.ts
@@ -2,6 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
+  // apiKey: "AIzaSyAylJ1IikuXz_rUSOj84looAZmXjzRc-eE",
+  // authDomain: "retailmind-ai-9db67.firebaseapp.com",
+  // projectId: "retailmind-ai-9db67",
+  // storageBucket: "retailmind-ai-9db67.firebasestorage.app",
+  // messagingSenderId: "935867070354",
+  // appId: "1:935867070354:web:3d62ba2a85eb839e03734b"
+
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
