155c7317346ef3e8804224c6186a7ef9ad73678d
Manvith-Kumar16 <manvithkumar1319@gmail.com>
2026-07-16 14:26:18 +0530

    feat: added all the setup build required

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -8,13 +8,6 @@ export default defineConfig({
     target: 'esnext',
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
     rollupOptions: {
       output: {
         manualChunks(id) {
